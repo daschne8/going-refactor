17d4cd726f07ee87c93fa74d7dfee6bd3367e52c
daschne8 <daschne8@gmail.com>
2019-07-10 11:51:02 -0400

    added tags to serialization and displays

--- app/assets/javascripts/good.js
+++ app/assets/javascripts/good.js
@@ -1,4 +1,4 @@
-// let goods_array = []
+let goods_array = []
 // class Good{
 //   constructor(data){
 //     this.id = data["id"];
@@ -32,15 +32,23 @@ function Good(data){
       this.name = data["name"];
       this.description = data["description"];
       this.occupant = data["occupant"]["name"];
+      this.tags = data["tags"].map(a => a.name)
+      goods_array.push(this)
 }
 
 Good.prototype.addGood = function(){
   let good_id = this.id
+  let tags_list = `<ul hidden id='good-${this.id}'>`
+  $.each(this.tags,function(index,name){
+    tags_list += `<li>${name}</li>`
+  })
+  tags_list += '</ul>'
   $('.goods-container').append(`<div class='good' id='good-${this.id}'>
     <a href="/goods/${this.id}">${this.name} - ${this.occupant}</a>
     <button class="remove" id="remove-${this.id}">Remove</button><br>
     <p hidden id='good-${this.id}'>${this.description}</p>
-    <br><br>
+    ${tags_list}
+    <br>
   </div>`);
   $(`button#remove-${this.id}`).click(function(){
     let id = parseInt($(this).attr('id').match(/\d+/)[0])
@@ -48,5 +56,6 @@ Good.prototype.addGood = function(){
   });
   $(`div#good-${this.id}`).click(function(){
     $(`div#good-${good_id} p`).toggle()
+    $(`div#good-${good_id} ul`).toggle()
   });
 }
